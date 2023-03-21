5e06a3d968e8e12621cd569a976ac42fba8ac2fd
aakarshan-raj <aakarshan7raj@gmail.com>
2023-03-21 12:03:45 +0530

    Added fully functional Toolbar that opens the navigation bar

--- app/src/main/java/com/example/learncpp_app/MainActivity.kt
+++ app/src/main/java/com/example/learncpp_app/MainActivity.kt
@@ -3,8 +3,11 @@ package com.example.learncpp_app
 import android.bluetooth.BluetoothCsipSetCoordinator
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.widget.Toolbar
 import androidx.coordinatorlayout.widget.CoordinatorLayout
+import androidx.core.view.GravityCompat
 import androidx.drawerlayout.widget.DrawerLayout
 import com.google.android.material.internal.NavigationMenu
 import com.google.android.material.navigation.NavigationView
@@ -23,10 +26,23 @@ class MainActivity : AppCompatActivity() {
         toolbar = findViewById(R.id.toolbar)
         navigationView = findViewById(R.id.navigationView)
         setToolBar()
+        var actionBarDrawerToggle = ActionBarDrawerToggle(this@MainActivity,drawerLayout,R.string.open_drawer,R.string.close_drawer)
+        drawerLayout.addDrawerListener(actionBarDrawerToggle)
+        actionBarDrawerToggle.syncState()
     }
 
     fun setToolBar(){
         setSupportActionBar(toolbar)
         supportActionBar?.title = "LearnCpp"
+        supportActionBar?.setHomeButtonEnabled(true)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        var icon = item.itemId
+        if(icon == android.R.id.home){
+            drawerLayout.openDrawer(GravityCompat.START)
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
