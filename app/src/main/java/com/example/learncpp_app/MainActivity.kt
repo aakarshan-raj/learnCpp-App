ec8330e09ebdb60294378743cb36230d2a19f51c
aakarshan-raj <aakarshan7raj@gmail.com>
2023-03-21 11:21:56 +0530

    connected the components to the kotlin

--- app/src/main/java/com/example/learncpp_app/MainActivity.kt
+++ app/src/main/java/com/example/learncpp_app/MainActivity.kt
@@ -1,11 +1,27 @@
 package com.example.learncpp_app
 
+import android.bluetooth.BluetoothCsipSetCoordinator
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.widget.Toolbar
+import androidx.coordinatorlayout.widget.CoordinatorLayout
+import androidx.drawerlayout.widget.DrawerLayout
+import com.google.android.material.internal.NavigationMenu
+import com.google.android.material.navigation.NavigationView
 
 class MainActivity : AppCompatActivity() {
+    lateinit var drawerLayout:DrawerLayout
+    lateinit var coordinatorLayout:CoordinatorLayout
+    lateinit var toolbar:Toolbar
+    lateinit var navigationView:NavigationView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        drawerLayout = findViewById(R.id.drawerLayout)
+        coordinatorLayout = findViewById(R.id.coordinatorLayout)
+        toolbar = findViewById(R.id.toolbar)
+        navigationView = findViewById(R.id.navigationView)
+
     }
 }
