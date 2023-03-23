2268f01b2c9909966044496062820f2f73c42964
aakarshan-raj <aakarshan7raj@gmail.com>
2023-03-23 10:10:32 +0530

    made changes to the fragments and implmented toolbar function

--- app/src/main/java/com/example/learncpp_app/MainActivity.kt
+++ app/src/main/java/com/example/learncpp_app/MainActivity.kt
@@ -30,69 +30,85 @@ class MainActivity : AppCompatActivity() {
         navigationView.setNavigationItemSelectedListener {
             when(it.itemId){
                 R.id.one->{
-                    supportFragmentManager.beginTransaction().replace(R.id.frame,Introduction()).commit()
+                    supportFragmentManager.beginTransaction().replace(R.id.frame,Introduction()).addToBackStack("one").commit()
+                    supportActionBar?.setTitle("Introduction to C++")
                     drawerLayout.closeDrawers()
                 }
 
                 R.id.two-> {
-                    supportFragmentManager.beginTransaction().replace(R.id.frame,VariablesData()).commit()
+                    supportFragmentManager.beginTransaction().replace(R.id.frame,VariablesData()).addToBackStack("two").commit()
+                    supportActionBar?.setTitle("Variables and Data Types")
                     drawerLayout.closeDrawers()
                 }
                 R.id.three->{
-                    supportFragmentManager.beginTransaction().replace(R.id.frame,OperatorsExpression()).commit()
+                    supportFragmentManager.beginTransaction().replace(R.id.frame,OperatorsExpression()).addToBackStack("three").commit()
+                    supportActionBar?.setTitle("Operators and Expressions")
                     drawerLayout.closeDrawers()
                 }
                 R.id.four->{
-                    supportFragmentManager.beginTransaction().replace(R.id.frame,ControlStructure()).commit()
+                    supportFragmentManager.beginTransaction().replace(R.id.frame,ControlStructure()).addToBackStack("four").commit()
+                    supportActionBar?.setTitle("Control Structures")
                     drawerLayout.closeDrawers()
                 }
                 R.id.five->{
-                    supportFragmentManager.beginTransaction().replace(R.id.frame,Functions()).commit()
+                    supportFragmentManager.beginTransaction().replace(R.id.frame,Functions()).addToBackStack("five").commit()
+                    supportActionBar?.setTitle("Functions")
                     drawerLayout.closeDrawers()
                 }
                 R.id.six->{
-                    supportFragmentManager.beginTransaction().replace(R.id.frame,ArraysPointer()).commit()
+                    supportFragmentManager.beginTransaction().replace(R.id.frame,ArraysPointer()).addToBackStack("six").commit()
+                    supportActionBar?.setTitle("Arrays and Pointers")
                     drawerLayout.closeDrawers()
                 }
                 R.id.seven->{
-                    supportFragmentManager.beginTransaction().replace(R.id.frame,ClassesObject()).commit()
+                    supportFragmentManager.beginTransaction().replace(R.id.frame,ClassesObject()).addToBackStack("seven").commit()
+                    supportActionBar?.setTitle("Classes and Objects")
                     drawerLayout.closeDrawers()
                 }
                 R.id.eight->{
-                    supportFragmentManager.beginTransaction().replace(R.id.frame,InheritancePoly()).commit()
+                    supportFragmentManager.beginTransaction().replace(R.id.frame,InheritancePoly()).addToBackStack("eight").commit()
+                    supportActionBar?.setTitle("Inheritance and Polymorphism")
                     drawerLayout.closeDrawers()
                 }
 
                 R.id.nine-> {
-                    supportFragmentManager.beginTransaction().replace(R.id.frame,Template()).commit()
+                    supportFragmentManager.beginTransaction().replace(R.id.frame,Template()).addToBackStack("nine").commit()
+                    supportActionBar?.setTitle("Templates")
                     drawerLayout.closeDrawers()
                 }
                 R.id.ten->{
-                    supportFragmentManager.beginTransaction().replace(R.id.frame,STL()).commit()
+                    supportFragmentManager.beginTransaction().replace(R.id.frame,STL()).addToBackStack("ten").commit()
+                    supportActionBar?.setTitle("STL")
                     drawerLayout.closeDrawers()
                 }
                 R.id.eleven->{
-                    supportFragmentManager.beginTransaction().replace(R.id.frame,InputOutput()).commit()
+                    supportFragmentManager.beginTransaction().replace(R.id.frame,InputOutput()).addToBackStack("eleven").commit()
+                    supportActionBar?.setTitle("Input and Output Streams")
                     drawerLayout.closeDrawers()
                 }
                 R.id.twelve->{
-                    supportFragmentManager.beginTransaction().replace(R.id.frame,Memory()).commit()
+                    supportFragmentManager.beginTransaction().replace(R.id.frame,Memory()).addToBackStack("twelve").commit()
+                    supportActionBar?.setTitle("Memory Management and Smart Pointers")
                     drawerLayout.closeDrawers()
                 }
                 R.id.thirteen->{
-                    supportFragmentManager.beginTransaction().replace(R.id.frame,Exception()).commit()
+                    supportFragmentManager.beginTransaction().replace(R.id.frame,Exception()).addToBackStack("thirteen").commit()
+                    supportActionBar?.setTitle("Exception Handling")
                     drawerLayout.closeDrawers()
                 }
                 R.id.fourteen->{
-                    supportFragmentManager.beginTransaction().replace(R.id.frame,Multithreading()).commit()
+                    supportFragmentManager.beginTransaction().replace(R.id.frame,Multithreading()).addToBackStack("fourteen").commit()
+                    supportActionBar?.setTitle("Multithreading and Concurrency")
                     drawerLayout.closeDrawers()
                 }
                 R.id.fifteen->{
-                    supportFragmentManager.beginTransaction().replace(R.id.frame,Debug()).commit()
+                    supportFragmentManager.beginTransaction().replace(R.id.frame,Debug()).addToBackStack("fifteen").commit()
+                    supportActionBar?.setTitle("Debugging Techniques and Tools")
                     drawerLayout.closeDrawers()
                 }
                 R.id.sixteen->{
-                    supportFragmentManager.beginTransaction().replace(R.id.frame,End()).commit()
+                    supportFragmentManager.beginTransaction().replace(R.id.frame,End()).addToBackStack("sixteen").commit()
+                    supportActionBar?.setTitle("End")
                     drawerLayout.closeDrawers()
                 }
             }
@@ -113,5 +129,10 @@ class MainActivity : AppCompatActivity() {
             drawerLayout.openDrawer(GravityCompat.START)
         }
         return super.onOptionsItemSelected(item)
+    }
+
+    override fun onBackPressed() {
+        super.onBackPressed()
+
     }
 }
